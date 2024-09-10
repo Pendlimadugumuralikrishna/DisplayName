7fbd7f642070d5410b3882909b2b5440e97d7908
unknown <pmmike2106@gmail.com>
2024-09-10 14:50:38 +0530

    2 tests fail commit

--- displayname/src/components/Displayname.jsx
+++ displayname/src/components/Displayname.jsx
@@ -11,6 +11,8 @@ function Displayname() {
         if(fullName.firstName !== "" && fullName.lastName!==""){
             console.log("in block");
             setVerified(true);
+        }else{
+            setVerified(false);
         }
 
     }
@@ -22,10 +24,10 @@ function Displayname() {
     <div>
         <form onSubmit={e=>handleSubmit(e)}>
             <label htmlFor="firstName">First Name:</label>
-            <input type="text" placeholder='firstname' id='firstName' name="firstName" onChange={e=>handleChange(e)}/>   <br/>
+            <input type="text" placeholder='firstname' required id='firstName' name="firstName" onChange={e=>handleChange(e)}/>   <br/>
             <label htmlFor="lastName">last Name:</label>
          
-            <input type="text" placeholder='lastName' id='lastName' name="lastName" onChange={e=>handleChange(e)}/>
+            <input type="text" placeholder='lastName' required id='lastName' name="lastName" onChange={e=>handleChange(e)}/>
             <br/>
             <button type='submit'>submit</button>
         </form>
